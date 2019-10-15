28da4bb953ae496b8727c96482a208084fa1142a
Wang Ziyi <9590738+wong-ziyi@users.noreply.github.com>
2019-10-15 19:11:31 -0400

    Update 02.Hisat2_AligingReads.sh

--- RNA-seq_GSE61351/02.Hisat2_AligingReads.sh
+++ RNA-seq_GSE61351/02.Hisat2_AligingReads.sh
@@ -9,5 +9,10 @@ do
   name1=$(find . -maxdepth ${level} -type f -name "*.fastq" | awk ${seq1})
   name=${name1%".fastq"*};
   echo "Mapping sample ${i} with ${core} CPU cores (${name}): ${name1}";
+  #Hisat2: Aliging reads
   hisat2 -p ${core} -x ./Hisat_Genome_Index_With_ss_exon/genome_tran -U ${name1} -S ${name}.sam
+  #Samtools sorting and converting sam to bam file
+  samtools sort -@ ${core} -o ${name}.bam ${name}.sam
+  #delete old unsorted sam file
+  rm ${name}.sam
 done
