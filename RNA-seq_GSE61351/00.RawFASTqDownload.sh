8972cc21e4f829f2050108d4d76398d251c7b776
Wang Ziyi <9590738+wong-ziyi@users.noreply.github.com>
2019-10-15 19:12:02 -0400

    Update 00.RawFASTqDownload.sh

--- RNA-seq_GSE61351/00.RawFASTqDownload.sh
+++ RNA-seq_GSE61351/00.RawFASTqDownload.sh
@@ -3,7 +3,7 @@ while getopts v:r:i:t:e: option
 do
 case "${option}"
 in
-v)        volume=${OPTARG};;
+v)   volume=${OPTARG};;
 r)      SRR=${OPTARG};;
 i)   Series=${OPTARG};;
 t)    Start=${OPTARG};;
@@ -14,5 +14,5 @@ for i in $(seq ${Start} ${End})
 do
 echo "Downloading ${SRR}${Series}${i}: ftp://ftp.sra.ebi.ac.uk/${volume}/fastq/${SRR}/$(printf "%03d" ${i})/${SRR}${Series}${i}/${SRR}${Series}${i}.fastq.gz";
 ascp -v -QT -l 300m -P33001 -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh era-fasp@fasp.sra.ebi.ac.uk:/${volume}/fastq/${SRR}/$(printf "%03d" ${i})/${SRR}${Series}${i}/${SRR}${Series}${i}.fastq.gz ./
-gunzip ${SRR}${Series}${i}.fastq.gz
+sudo gunzip ${SRR}${Series}${i}.fastq.gz
 done
